873f98ad7700aeda0ea73fd3860a2c4b823d8909
MalgorzataEljaszukSanchez <malgorzata.sanchez.art@gmail.com>
2024-04-29 18:34:30 +0200

    button 'Generate Question' added

--- scripts.js
+++ scripts.js
@@ -1,13 +1,38 @@
-let url = './source.txt';
-// Zawartość pliku tekstowego w formie ciągu znaków
-var tekstPliku = `To jest pytanie.
-To jest odpowiedź.`;
+let url = 'source.txt';
+let numberOfRow = 4;
+let orderOfAnswers = [];
 
-// Podział ciągu na osobne wiersze
-var wiersze = tekstPliku.split('\n');
+// Pobranie referencji do przycisku
+let button = document.getElementById("generateButton");
 
-// Załaduj pierwszy wiersz do .app__question
-document.querySelector('.app__question').textContent = wiersze[0];
+// Dodanie obsługi zdarzenia kliknięcia do przycisku
+button.addEventListener("click", generateQuestion);
 
-// Załaduj drugi wiersz do .app__answer
-document.querySelector('.app__answer').textContent = wiersze[1];
+
+
+function generateQuestion(numberOfRow) {
+// Pobranie zawartości pliku tekstowego
+
+fetch(url)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Nie można pobrać zawartości pliku.');
+        }
+        return response.text();
+    })
+    .then(data => {
+        // Podział zawartości pliku na osobne wiersze
+        var wiersze = data.split('\n');
+
+        // Załaduj pierwszy wiersz do .app__question
+        document.querySelector('.app__question').textContent = wiersze[numberOfRow];
+
+        // Załaduj drugi wiersz do .app__answer
+        document.querySelector('.app__answer--1').textContent = wiersze[numberOfRow+1];
+        document.querySelector('.app__answer--2').textContent = wiersze[numberOfRow+2];
+        document.querySelector('.app__answer--3').textContent = wiersze[numberOfRow+3];
+    })
+    .catch(error => {
+        console.error('Wystąpił błąd:', error);
+    })
+};
