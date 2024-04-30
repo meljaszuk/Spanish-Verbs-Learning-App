2280a8b8fe92d9eb432a8891a19e1cbbb14c3627
MalgorzataEljaszukSanchez <malgorzata.sanchez.art@gmail.com>
2024-04-30 14:33:09 +0200

    question generator working correctly

--- scripts.js
+++ scripts.js
@@ -1,14 +1,31 @@
-let url = 'source.txt';
-let multiplier = 1+ Math.floor(Math.random() * 5);
-console.log(multiplier)
-let numberOfRow = 4 * multiplier;
+
+'use strict'
+const NUMBER_OF_QUESTIONS = 5;
+let numberOfRow;
 let selectedQuestion ='';
 let answers = [];
 let wiersze;
 
 // Pobranie zawartości pliku tekstowego */
 
-fetch(url)
+function getNumberOfRowOfQuestion() {
+    let multiplier = 1+ Math.floor(Math.random() * NUMBER_OF_QUESTIONS);
+    numberOfRow = 1+ 4 * multiplier;
+
+    console.log('mulitplier', multiplier)
+    console.log('number of row of question', numberOfRow)
+
+}
+
+function displayQuestion() {
+
+    getNumberOfRowOfQuestion();
+console.log('number of row of question', numberOfRow)
+    
+
+let url = 'source.txt';
+  
+    fetch(url)
     .then(response => {
         if (!response.ok) {
             console.log('error')
@@ -19,26 +36,31 @@ fetch(url)
     .then(data => {
         // Podział zawartości pliku na osobne wiersze
         wiersze = data.split('\n');
-        selectedQuestion = wiersze[numberOfRow];
-        console.log('assigned',selectedQuestion);
-        answers = [wiersze[numberOfRow+1], wiersze[numberOfRow+2], wiersze[numberOfRow+3]];
+        selectedQuestion = wiersze[numberOfRow-1];
+
+console.log(wiersze)
+
+
+
+
+        console.log(wiersze[numberOfRow-1], 'quest')
+
+        answers = [wiersze[numberOfRow], wiersze[numberOfRow+1], wiersze[numberOfRow+2]];
         console.log(answers);
-        
+        document.querySelector('.app__question').textContent = selectedQuestion;
+
+ // Załaduj drugi wiersz do .app__answer
+ document.querySelector('.app__answer--1').textContent = answers[0];
+ document.querySelector('.app__answer--2').textContent = answers[1];
+ document.querySelector('.app__answer--3').textContent = answers[2];
        
     })
     .catch(error => {
         console.error('Wystąpił błąd:', error);
     });
 
-function displayQuestion(numberOfRow) {
-console.log('function geenrate Question has been called')
  // Załaduj pierwszy wiersz do .app__question
- document.querySelector('.app__question').textContent = selectedQuestion;
-
- // Załaduj drugi wiersz do .app__answer
- document.querySelector('.app__answer--1').textContent = wiersze[numberOfRow+1];
- document.querySelector('.app__answer--2').textContent = wiersze[numberOfRow+2];
- document.querySelector('.app__answer--3').textContent = wiersze[numberOfRow+3];
+ 
 }
 
 // Pobranie referencji do przycisku
